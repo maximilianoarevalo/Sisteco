161eefe9094f30cd01da07ed82bb19a06ded07e9
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2020-09-17 15:33:44 -0300

    Playfair Regla 1

--- Lab3/playfair.py
+++ Lab3/playfair.py
@@ -45,6 +45,64 @@ def modifyWord(message):
 		newMessage[i] = newMessage[i].upper()
 	return newMessage
 
+def findInMatrix(letter,playfairMatrix):
+	rowIterator = 0;
+	columnIterator = 0;
+	rows = 5;
+	columns = 5;
+	ubication = []
+	while(rowIterator < rows):
+		while(columnIterator < columns):
+			if letter == playfairMatrix[rowIterator][columnIterator]:
+				ubication.append(rowIterator);
+				ubication.append(columnIterator);
+			columnIterator = columnIterator + 1
+		columnIterator = 0
+		rowIterator = rowIterator + 1
+	return ubication
+
+def encrypt(modifyMessage,playfairMatrix):
+	encrypted = ""
+	for pairLetters in modifyMessage:
+		positions = []
+		#Get the index
+		for letter in pairLetters:
+			positions.append(findInMatrix(letter,playfairMatrix))
+		print(positions)
+		print("New positions:")
+		#Rules
+		#Rule 1: If m1 and m2 are in the same row -> c1 and c2 located on the right (circular)
+		if positions[0][0] == positions[1][0]:
+			positions[0][1] = positions[0][1] + 1
+			positions[1][1] = positions[1][1] + 1
+			a = positions[0][0]
+			b = positions[0][1]
+			c = positions[1][0]
+			d = positions[1][1]
+			#Index validation
+			if a == 5:
+				a = 0
+			elif b == 5:
+				b = 0
+			elif c == 5:
+				c = 0
+			elif d == 5:
+				d = 0
+			#print(positions)
+			encrypted = encrypted + playfairMatrix[a][b]
+			encrypted = encrypted + playfairMatrix[c][d]
+			#print("Encrypted: "+encrypted)
+			break;
+
+	return encrypted		
+	
+
+	#Rule 2: If m1 and m2 are in the same column -> c1 and c2 located under (circular)
+
+	#Rule 3: If m1 and m2 are in different row and column -> c1 and c2 opposite diagonal
+
+	#Rule 4: If m1 = m2, add caracter between m1 and m2, and then applicate the 1-3 rules
+
 
 #Funcionamiento original
 #key = input("Ingrese la clave para el cifrado playfair: ")
@@ -53,6 +111,11 @@ def modifyWord(message):
 #modifiedWord= modifyWord(palabra)
 key = "monarchy"
 matriz = createMatrix(key)
-palabra = "maximiliano"
+palabra = "mrximiliano"
 A = modifyWord(palabra)
-print(A)
+#encryptedText = encrypt(modifiedWord,matrix)
+#print(A)
+#ubicacion = findInMatrix("X",matriz)
+printMatrix(matriz)
+#print(ubicacion)
+encrypt(A,matriz)
