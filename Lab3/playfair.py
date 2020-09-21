367e6b03bc4aa7a387e60ba7d180df812e6360e5
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2020-09-21 17:43:01 -0300

    Arreglos finales

--- Lab3/playfair.py
+++ Lab3/playfair.py
@@ -1,4 +1,7 @@
-# Playfair Cipher
+## Playfair Cipher ##
+
+import time
+import matplotlib.pyplot as plt
 
 #Entrada: Una palabra que representa la llave para encriptar y desencriptar
 #Procesamiento: En base a la llave se crea una matriz de 5x5 donde se ingresan las letras
@@ -7,6 +10,7 @@ def createMatrix(key):
 	rows = 5
 	columns = 5
 	playfairMatrix = []
+	key = key.replace(" ", "")
 	#Define all leters without the j
 	allLetters = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 	#Now we remove the repeated letters in the key
@@ -14,7 +18,7 @@ def createMatrix(key):
 	for letter in key:
 		if letter not in keyNoRepeated:
 			keyNoRepeated = keyNoRepeated + letter
-	#We add all the other letters to the keyNoRepeated string
+	#Now we add all the other letters to the keyNoRepeated string
 	for letters in allLetters:
 		if letters not in keyNoRepeated:
 			keyNoRepeated = keyNoRepeated + letters
@@ -231,6 +235,7 @@ def playfairCrypt():
 	matrix = createMatrix(encryptKey)
 	modifiedWord= modifyWord(word)
 	encryptedText = encrypt(modifiedWord,matrix)
+	print("")
 	print("La palabra "+word+" encriptada en cifrado Playfair con llave "+encryptKey+" es: "+encryptedText)
 
 #Entrada: No tiene entrada directa, se solicitan por consola
@@ -240,25 +245,72 @@ def playfairDecrypt():
 	decryptKey = input("Ingrese la clave para desencriptar en cifrado Playfair: ")
 	cryptedWord = input("Ingrese la palabra encriptada en cifrado Playfair: ")
 	realWord = decrypt(cryptedWord,decryptKey)
+	print("")
 	print("La palabra "+cryptedWord+" desencriptada en cifrado Playfair con llave "+decryptKey+" es: "+realWord)	
+
+def graphPlot(title, x, y, xlabel, ylabel):
+	plt.title(title)
+	plt.xlabel(xlabel)
+	plt.ylabel(ylabel)
+	plt.plot(x, y, color='green', linestyle='dashed', linewidth = 3, marker='o', markerfacecolor='blue', markersize=5)
+	plt.show()
+
+#Entrada: Dos mensajes a encriptar, idealmente con un bit de diferencia y la llave de encriptacion
+#Procesamiento: Encripta dos mensajes en base a una llave común y muestra los resultados de encriptación como palabra y bits
+#Salida: Mensajes encriptados en formato de caracteres y en formato de bits
+def avalancheTest(message1,message2,key):
+	m1 = message1
+	bitsm1 = ' '.join(format(x, 'b') for x in bytearray(m1, 'utf-8'))
+	m2 = message2
+	bitsm2 = ' '.join(format(x, 'b') for x in bytearray(m2, 'utf-8'))
+	#For the first message, example: "hola"
+	matrix1 = createMatrix(key)
+	modifiedM1 = modifyWord(m1)
+	encryptedM1 = encrypt(modifiedM1,matrix1)
+	out1 = ' '.join(format(x, 'b') for x in bytearray(m1, 'utf-8'))
+	print("La palabra "+m1+" encriptada con llave "+key+" tiene como resultado "+encryptedM1+", cuyos bits son: "+out1)
+	#For the second message, example: "hole"
+	matrix2 = createMatrix(key)
+	modifiedM2 = modifyWord(m2)
+	encryptedM2 = encrypt(modifiedM2,matrix2)
+	out2 = ' '.join(format(x, 'b') for x in bytearray(m2, 'utf-8'))
+	print("La palabra "+m2+" encriptada con llave "+key+" tiene como resultado "+encryptedM2+", cuyos bits son: "+out2)
 
 #Entrada: No tiene entrada directa, se solicitan por consola
 #Procesamiento: Encripta o desencripta palabras utilizando cifrado Playfair
 #Salida: Palabra encriptada o desencriptada utilizando cifrado Playfair
 def menu():
+	#Block size is 16 bytes, because the characters of the message are grouped in pairs
+	#The thoughput must be in Kilobytes, so the block size is 0.015625 Kilobytes
 	print("### Cifrado Playfair ###")
+	print("Considere que ni la llave ni el mensaje pueden contener números debido a la naturaleza del encriptado Playfair")
 	print("Seleccione una de las siguientes opciones: ")
 	print("1. Encriptar")
 	print("2. Desencriptar")
 	print("3. Salir")
 	opcion = input("Ingrese su opción: ")
 	off = False
+	blockSize = 0.015625
 	while not off:
 		if opcion == "1":
+			#Playfair encrypt
+			start_time = time.time()
 			playfairCrypt()
+			end_time = time.time()
+			encryptTime = end_time - start_time
+			throughput = blockSize / encryptTime
+			print("El tiempo de encriptación es de: "+str(encryptTime)+" segundos y el Throughput es: "+str(throughput))
+			graphPlot("Throughput V/S Tamaño de bloque de mensaje para encriptación", throughput, blockSize, "Throughput","Tamaño de bloque [Kilobytes]")
 			off = True
 		elif opcion == "2":
+			#Playfair decrypt
+			start_time = time.time()
 			playfairDecrypt()
+			end_time = time.time()
+			decryptTime = end_time - start_time
+			throughput = blockSize / decryptTime
+			print("El tiempo de desencriptación es de: "+str(decryptTime)+" segundos y el Throughput es: "+str(throughput))
+			graphPlot("Throughput V/S Tamaño de bloque de mensaje para desencriptación", throughput, blockSize, "Throughput","Tamaño de bloque [Kilobytes]")
 			off = True
 		elif opcion == "3":
 			off = True
@@ -270,3 +322,5 @@ def menu():
 
 #Cifrado Playfair
 menu()
+#Just test for the avalanche effect
+#avalancheTest("hola","hole","clave")
